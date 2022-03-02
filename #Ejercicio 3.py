43772c199818ea8c4ba3ea6c83db14d3d9f52e72
carlospuigserver <cpuiggut@myuax.com>
2022-03-02 17:46:24 +0100

    subida

--- #Ejercicio 3.py
+++ #Ejercicio 3.py
@@ -5,3 +5,9 @@ t=[]
 i=0
 fichas=["Verdes", "Azules", "Rojas"]
 cantidad=int(input("Escribe con cuantas fichas quieres jugar:  "))
+def tablero(t,i,fichas,cantidad):
+    i+=1
+    if i <= cantidad:
+        t.append[randint(0,2)]
+        tablero(t,i,fichas,cantidad)
+tablero(t,i,fichas,cantidad)
