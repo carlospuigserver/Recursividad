f0d31133dfc20935955145e71a9a128aba957e05
carlospuigserver <cpuiggut@myuax.com>
2022-03-02 16:18:39 +0100

    SUBIDA

--- #Ejercicio 2.py
+++ #Ejercicio 2.py
@@ -3,3 +3,11 @@
 palabra=input ("Escribe una palabra a analizar:  ")
 capicua=""
 p=len(palabra)
+
+while p>=0: #Concateno las letras una por una y las guardo
+    p=p-1
+    capicua=capicua+palabra[p]
+if palabra ==capicua:
+        print("Es polindroma")
+else:
+        print("No lo es")
